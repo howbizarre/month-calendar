2db481505fe19f4d6463b3ffc55083d8c2f635f1
howbizarre <howbizarre@gmail.com>
2023-01-11 13:07:15 +0200

    UPD: Its working...

--- src/components/MonthActions.vue
+++ src/components/MonthActions.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex justify-between p-3">
-      <button @click="$emit('prevMonth')">
+      <button @click="prevMonth">
         <svg width="24" height="24" viewBox="0 0 256 256" class="action-icon">
           <path fill="currentColor" d="m120 32l-96 96l96 96v-48h32V80h-32V32z" opacity=".2" />
           <path fill="currentColor" d="M152 72h-24V32a8 8 0 0 0-4.9-7.4a8.4 8.4 0 0 0-8.8 1.7l-96 96a8.1 8.1 0 0 0 0 11.4l96 96a8.3 8.3 0 0 0 5.7 2.3a8.5 8.5 0 0 0 3.1-.6a8 8 0 0 0 4.9-7.4v-40h24a8 8 0 0 0 8-8V80a8 8 0 0 0-8-8Zm-8 96h-24a8 8 0 0 0-8 8v28.7L35.3 128L112 51.3V80a8 8 0 0 0 8 8h24Zm80-88v96a8 8 0 0 1-16 0V80a8 8 0 0 1 16 0Zm-32 0v96a8 8 0 0 1-16 0V80a8 8 0 0 1 16 0Z" />
         </svg>
       </button>
-      <button @click="$emit('resetMonth')">
+      <button @click="resetMonth">
         <svg width="24" height="24" viewBox="0 0 256 256" class="action-icon">
           <path fill="currentColor" d="M195.9 195.9a96.1 96.1 0 0 1-135.8 0a8 8 0 0 1 0-11.3a7.9 7.9 0 0 1 11.3 0a80 80 0 1 0 0-113.2L51.1 91.7h28.7a8 8 0 0 1 0 16h-48a8 8 0 0 1-8-8v-48a8 8 0 1 1 16 0v28.7l20.3-20.3a96 96 0 0 1 135.8 135.8Z" />
         </svg>
       </button>
-      <button @click="$emit('nextMonth')">
+      <button @click="nextMonth">
         <svg width="24" height="24" viewBox="0 0 256 256" class="action-icon">
           <path fill="currentColor" d="m136 32l96 96l-96 96v-48h-32V80h32V32z" opacity=".2" />
           <path fill="currentColor" d="m237.7 122.3l-96-96a8.4 8.4 0 0 0-8.8-1.7A8 8 0 0 0 128 32v40h-24a8 8 0 0 0-8 8v96a8 8 0 0 0 8 8h24v40a8 8 0 0 0 4.9 7.4a8.5 8.5 0 0 0 3.1.6a8.3 8.3 0 0 0 5.7-2.3l96-96a8.1 8.1 0 0 0 0-11.4ZM144 204.7V176a8 8 0 0 0-8-8h-24V88h24a8 8 0 0 0 8-8V51.3l76.7 76.7ZM48 80v96a8 8 0 0 1-16 0V80a8 8 0 0 1 16 0Zm32 0v96a8 8 0 0 1-16 0V80a8 8 0 0 1 16 0Z" />
@@ -19,6 +19,14 @@
       </button>
     </div>
 </template>
+
+<script lang="ts" setup>
+const emit = defineEmits(['prevMonth', 'resetMonth', 'nextMonth']);
+
+const prevMonth = () => emit('prevMonth');
+const resetMonth = () => emit('resetMonth');
+const nextMonth = () => emit('nextMonth');
+</script>
 
 <style>
 .action-icon {
--- src/components/MonthDays.vue
+++ src/components/MonthDays.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calendar-grid py-3">
     <div v-for="md in allDaysInMonth" class="text-center">
-      <button class="calendar-day" :class="md.day === 'sat' || md.day === 'sun' ? 'weekend' : ''">
+      <button :data-info="[md.day, md.date, md.month, md.year]" class="calendar-day" :class="md.day === 'sat' || md.day === 'sun' ? 'weekend' : ''">
         {{ md.date }}
       </button>
     </div>
@@ -13,8 +13,9 @@ import { ref, watch } from "vue";
 import { monthName, numberOfDaysInMonth, dayOfWeek } from "typescript-calendar-date";
 import { weekDays, totalDaysInMonthGrid } from "@/utils/date-processing";
 
-import type { CalendarDate, CalendarMonth, CalendarYear } from "typescript-calendar-date";
+import type { CalendarMonth, CalendarYear } from "typescript-calendar-date";
 import type { WeekFirstDay } from "@/utils/date-processing";
+import type { Month } from "typescript-calendar-date/dist/consts";
 
 const props = defineProps<{
   startDay: WeekFirstDay;
@@ -22,77 +23,88 @@ const props = defineProps<{
   year: number;
 }>();
 
-const nameOfMonth = monthName(props.month);
-
-const viewYear: CalendarYear = {
-  year: props.year
-};
-const viewMonth: CalendarMonth = {
-  ...viewYear,
-  month: nameOfMonth
-};
-const currentDay: CalendarDate = {
-  ...viewMonth,
-  day: new Date().getDate(),
+type MonthArray = {
+  day: string;
+  date: number;
+  month: Month;
+  year: number;
 };
 
-const countDaysInViewMonth: number = numberOfDaysInMonth(viewMonth);
-const countDaysInPrevMonth: number = numberOfDaysInMonth({ year: props.month === 1 ? props.year - 1 : props.year, month: monthName(props.month - 1) });
-const countDaysInNextMonth: number = numberOfDaysInMonth({ year: props.month === 12 ? props.year + 1 : props.year, month: monthName(props.month + 1) });
+const allDaysInMonth = ref(fillMonth(props.month, props.year, props.startDay));
 
-const firstWeekDayInMonth = dayOfWeek({ ...viewMonth, day: 1 });
-const daysOffsets = weekDays[props.startDay].short.indexOf(firstWeekDayInMonth);
-const daysPreset = totalDaysInMonthGrid - (daysOffsets + countDaysInViewMonth);
+watch(props, () => {
+  allDaysInMonth.value = fillMonth(props.month, props.year, props.startDay);
+});
 
-const allDaysInMonth = ref(new Array());
+function fillMonth(month: number, year: number, firstDayOfTheWeek: WeekFirstDay): MonthArray[] {
+  const fullFiledArray = new Array();
 
-let dOff = daysOffsets;
-let dPrs = 7 - daysPreset;
-
-for (let i = 0; i < daysOffsets; i += 1) {
-  const date = (countDaysInPrevMonth + (i + 1)) - daysOffsets;
-  const day = weekDays[props.startDay].short[i];
-  const month = monthName(props.month - 1);
-  const year = props.month === 1 ? props.year - 1 : props.year;
-
-  allDaysInMonth.value.push({
-    day: day,
-    date: date,
-    month: month,
-    year: year  
-  });
-}
-
-for (let i = 0; i < countDaysInViewMonth; i += 1) {
-  const date = i + 1;
-  const day = weekDays[props.startDay].short[dOff];
-  const month = monthName(props.month);
-  const year = props.year;
-
-  dOff = dOff > 5 ? 0 : dOff + 1;
-
-  allDaysInMonth.value.push({
-    day: day,
-    date: date,
-    month: month,
+  const nameOfMonth = monthName(month);
+  const viewYear: CalendarYear = {
     year: year
-  });
-}
+  };
+  const viewMonth: CalendarMonth = {
+    ...viewYear,
+    month: nameOfMonth
+  };
 
-for (let i = 0; i < daysPreset; i += 1) {
-  const date = i + 1;
-  const day = weekDays[props.startDay].short[dPrs];
-  const month = monthName(props.month + 1);
-  const year = props.month === 12 ? props.year + 1 : props.year;
+  const firstWeekDayInMonth = dayOfWeek({ ...viewMonth, day: 1 });
+  const countDaysInViewMonth = numberOfDaysInMonth(viewMonth);
+  const countDaysInPrevMonth = numberOfDaysInMonth({ year: month === 1 ? year - 1 : year, month: monthName(month - 1) });
 
-  dPrs = dPrs > 5 ? 0 : dPrs + 1;
+  const daysOffsets = weekDays[firstDayOfTheWeek].short.indexOf(firstWeekDayInMonth);
+  const daysPreset = totalDaysInMonthGrid - (daysOffsets + countDaysInViewMonth);
 
-  allDaysInMonth.value.push({
-    day: day,
-    date: date,
-    month: month,
-    year: year
-  });
+  let dOff = daysOffsets;
+  let dPrs = daysPreset < 7 ? 7 - daysPreset : 14 - daysPreset;
+
+  for (let i = 0; i < daysOffsets; i += 1) {
+    const date = (countDaysInPrevMonth + (i + 1)) - daysOffsets;
+    const day = weekDays[firstDayOfTheWeek].short[i];
+    const mnth = monthName(month - 1);
+    const yr = month === 1 ? year - 1 : year;
+
+    fullFiledArray.push({
+      day: day,
+      date: date,
+      month: mnth,
+      year: yr
+    });
+  }
+
+  for (let i = 0; i < countDaysInViewMonth; i += 1) {
+    const date = i + 1;
+    const day = weekDays[firstDayOfTheWeek].short[dOff];
+    const mnth = monthName(month);
+    const yr = year;
+
+    dOff = dOff > 5 ? 0 : dOff + 1;
+
+    fullFiledArray.push({
+      day: day,
+      date: date,
+      month: mnth,
+      year: yr
+    });
+  }
+
+  for (let i = 0; i < daysPreset; i += 1) {
+    const date = i + 1;
+    const day = weekDays[firstDayOfTheWeek].short[dPrs];
+    const mnth = monthName(month + 1);
+    const yr = month === 12 ? year + 1 : year;
+
+    dPrs = dPrs > 5 ? 0 : dPrs + 1;
+    
+    fullFiledArray.push({
+      day: day,
+      date: date,
+      month: mnth,
+      year: yr
+    });
+  }
+
+  return fullFiledArray;
 }
 </script>
 
