1ef2d5f73e32abe0df740a4f8202f6377e190b1e
howbizarre <howbizarre@gmail.com>
2023-01-18 17:19:34 +0200

    ADD: Vume Month Calendar package

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <Month />
+  <VueMonthCalendar @get-date="getDate" />
 </template>
 
 <script lang="ts" setup>
-import Month from "@/components/Month.vue";
+import { VueMonthCalendar } from "@howbizarre/vue-month-calendar";
+import "@howbizarre/vue-month-calendar/dist/style.css";
+
+function getDate(activeDate: { month: number, year: number, date: number }) {
+  console.group("The returned data is:");
+  console.log("Day: ", activeDate.date);
+  console.log("Month: ", activeDate.month);
+  console.log("Yeaar: ", activeDate.year);
+  console.groupEnd();
+}
 </script>
