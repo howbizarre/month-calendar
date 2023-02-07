297ca34c7218bd9bf668d7e46db462ce60918f71
howbizarre <howbizarre@gmail.com>
2023-02-07 16:59:04 +0200

    UPD: Calendar package

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <VueMonthCalendar @get-date="getDate" />
+  <VueMonthCalendar @get-date="getDate" :setEvents="setEvents" />
 </template>
 
 <script lang="ts" setup>
 import { VueMonthCalendar } from "@howbizarre/vue-month-calendar";
 import "@howbizarre/vue-month-calendar/dist/style.css";
+
+const date = new Date();
+const currentMonth = date.getMonth();
+const currentYear = date.getFullYear();
+
+console.log(date.getMonth())
+
+const setEvents = [
+  { date: 28, month: currentMonth, year: currentYear },
+  { date: 5, month: currentMonth + 1, year: currentYear },
+  { date: 5, month: currentMonth + 1, year: currentYear },
+  { date: 5, month: currentMonth + 1, year: currentYear },
+  { date: 14, month: currentMonth + 1, year: currentYear },
+  { date: 14, month: currentMonth + 1, year: currentYear },
+  { date: 10, month: currentMonth + 1, year: currentYear },
+  { date: 17, month: currentMonth + 1, year: currentYear },
+  { date: 22, month: currentMonth + 1, year: currentYear },
+  { date: 22, month: currentMonth + 1, year: currentYear },
+  { date: 23, month: currentMonth + 1, year: currentYear },
+  { date: 28, month: currentMonth + 1, year: currentYear },
+  { date: 1, month: currentMonth + 2, year: currentYear },
+  { date: 1, month: currentMonth + 2, year: currentYear },
+  { date: 3, month: currentMonth + 2, year: currentYear }
+];
 
 function getDate(activeDate: { month: number, year: number, date: number }) {
   console.group("The returned data is:");
