25eb44ad3ca9a0554a2c09d519e2addf3bd6637b
How Bizarre <howbizarre@gmail.com>
2023-03-21 17:42:22 +0200

    ADD: LogRocket

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 import { VueMonthCalendar } from "@howbizarre/vue-month-calendar";
 import "@howbizarre/vue-month-calendar/dist/style.css";
 
+import LogRocket from 'logrocket';
+LogRocket.init(`${import.meta.env.VITE_LOGROCKET_ID}`);
+
 const date = new Date();
 const currentMonth = date.getMonth();
 const currentYear = date.getFullYear();
