a961af082c0d4881c895a0e480017b1387cb65ec
How Bizarre <howbizarre@gmail.com>
2024-03-15 13:52:02 +0200

    Update dependencies and configuration files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
-<template>
-  <VueMonthCalendar
-                    @get-date="getDate"
-                    @change-year="changeYear"
-                    @change-month="changeMonth"
-                    @decrement-year="decrementYear"
-                    @increment-year="incrementYear"
-                    @next-month="nextMonth"
-                    @prev-month="prevMonth"
-                    @reset-month="resetMonth"
-                    @change-first-week-day="changeFirstWeekDay"
-
-                    :setEvents="setEvents" />
-</template>
-
 <script lang="ts" setup>
 import { VueMonthCalendar } from "@howbizarre/vue-month-calendar";
 import "@howbizarre/vue-month-calendar/dist/style.css";
@@ -79,3 +64,49 @@ function changeFirstWeekDay(firstWeekDay: number): void {
   console.log("Change First Week Day: ", firstWeekDay);
 }
 </script>
+
+<template>
+  <VueMonthCalendar
+                    @get-date="getDate"
+                    @change-year="changeYear"
+                    @change-month="changeMonth"
+                    @decrement-year="decrementYear"
+                    @increment-year="incrementYear"
+                    @next-month="nextMonth"
+                    @prev-month="prevMonth"
+                    @reset-month="resetMonth"
+                    @change-first-week-day="changeFirstWeekDay"
+
+                    :setEvents="setEvents" />
+</template>
+
+<style>
+:root {
+  --white: #fefefe;
+  --black: #3c3c3c;
+
+  --zinc-400: #a1a1aa;
+  --zinc-600: #52525b;
+  --zinc-600-25: #52525b25;
+  --zinc-800: #27272a;
+  --zinc-900: #18181b;
+  --zinc-900-5: #18181b05;
+  --zinc-900-50: #18181b50;
+
+  --teal-400: #2dd4bf;
+  --teal-500: #14b8a6;
+  --teal-500-25: #14b8a625;
+  --teal-600: #0891b2;
+  --teal-600-50: #0891b250;
+  --teal-700: #0e7490;
+  --teal-800: #155e75;
+
+  --sky-500: #0ea5e9;
+  --sky-500-5: #0ea5e905;
+  --sky-500-25: #0ea5e925;
+
+  --blue-600: #c93b10;
+
+  --container-sm-w: 420px;
+}
+</style>
